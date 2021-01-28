c667abedaebacadac59a6a8114946f94d8279f8a
unknown <delettre.aymerich@gmail.com>
2021-01-28 20:51:59 +0100

    Added create-post handling, global error handling, isAuth middleware, updated general layout

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -3,6 +3,7 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
@@ -10,18 +11,24 @@ import React, { InputHTMLAttributes } from "react";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textaera?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textaera,
   size: _,
   ...props
 }) => {
+  let InputOrTextaera = Input;
+  if (textaera) {
+    InputOrTextaera = Textarea;
+  }
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextaera {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
---
+++ src/pages/create-post.tsx
@@ -0,0 +1,58 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Layout } from "../components/Layout";
+import { useCreatePostMutation } from "../generated/graphql";
+import { createUrlqClient } from "../utils/createUrqlClient";
+import { useIsAuth } from "../utils/useIsAuth";
+
+const CreatePost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  useIsAuth();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values });
+          console.log("error: " + error);
+          if (!error) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="title"
+              label="Title"
+              placeholder="Title"
+            ></InputField>
+            <Box mt={4}>
+              <InputField
+                textaera
+                name="text"
+                label="Body"
+                placeholder="kokofruit"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              colorScheme="teal"
+            >
+              Create Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrlqClient)(CreatePost);
